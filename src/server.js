35738935906e00fe4d633e80e75a9b4b206fc67b
Lilian Lee <lilianlee0213@gmail.com>
2023-01-27 15:12:28 -0500

    Middlewares

--- src/server.js
+++ src/server.js
@@ -3,14 +3,31 @@ import express from 'express';
 const PORT = 4000;
 const app = express();
 
+const logger = (req, res, next) => {
+	console.log(`${req.method},${req.url}`);
+	next();
+};
+const privateMiddleWare = (req, res, next) => {
+	const url = req.url;
+	if (url === '/protected') {
+		return res.send('<h1>Not Allowed</h1>');
+	}
+	console.log('allowed');
+	next();
+};
 const handleHome = (req, res) => {
-	return res.send('I love you');
+	return res.send('I love middlewares');
 };
-const handleLogin = (req, res) => {
-	return res.send('login');
+const handleProtected = (req, res) => {
+	return res.send('welcome tpo the private lounge');
 };
+// const handleLogin = (req, res) => {
+// 	return res.send('login');
+// };
+app.use(logger);
+app.use(privateMiddleWare);
 app.get('/', handleHome);
-app.get('/login', handleLogin);
+app.get('/protected', handleProtected);
 
 app.listen(PORT, () =>
 	console.log(`server is listening on port http://localhost:${PORT}`)
