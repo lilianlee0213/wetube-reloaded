ecb0aceb2b554522f8b9b30165317ae07181b417
Lilian Lee <lilianlee0213@gmail.com>
2023-01-31 13:37:52 -0500

    set up routers and controllers

--- src/server.js
+++ src/server.js
@@ -1,33 +1,18 @@
 import express from 'express';
+import morgan from 'morgan';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const PORT = 4000;
-const app = express();
 
-const logger = (req, res, next) => {
-	console.log(`${req.method},${req.url}`);
-	next();
-};
-const privateMiddleWare = (req, res, next) => {
-	const url = req.url;
-	if (url === '/protected') {
-		return res.send('<h1>Not Allowed</h1>');
-	}
-	console.log('allowed');
-	next();
-};
-const handleHome = (req, res) => {
-	return res.send('I love middlewares');
-};
-const handleProtected = (req, res) => {
-	return res.send('welcome tpo the private lounge');
-};
-// const handleLogin = (req, res) => {
-// 	return res.send('login');
-// };
+const app = express();
+const logger = morgan('dev');
 app.use(logger);
-app.use(privateMiddleWare);
-app.get('/', handleHome);
-app.get('/protected', handleProtected);
+
+app.use('/', globalRouter);
+app.use('/users', userRouter);
+app.use('/videos', videoRouter);
 
 app.listen(PORT, () =>
 	console.log(`server is listening on port http://localhost:${PORT}`)
