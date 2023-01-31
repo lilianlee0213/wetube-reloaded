c04451bc448493667589d050e7548c049a18df16
Lilian Lee <lilianlee0213@gmail.com>
2023-01-31 13:38:34 -0500

    set up routers and controllers

---
+++ src/controllers/userController.js
@@ -0,0 +1,7 @@
+export const join = (req, res) => res.send('Join');
+export const login = (req, res) => res.send('Login');
+export const search = (req, res) => res.send('Search');
+export const logout = (req, res) => res.send('Logout');
+export const see = (req, res) => res.send('See');
+export const edit = (req, res) => res.send('Edit users');
+export const remove = (req, res) => res.send('Remove users');
---
+++ src/controllers/videoController.js
@@ -0,0 +1,11 @@
+export const trending = (req, res) => {
+	return res.send('Homepage trending');
+};
+export const see = (req, res) => {
+	return res.send('Watch');
+};
+export const edit = (req, res) => {
+	return res.send('Edit');
+};
+export const deleteVideo = (req, res) => res.send('Delete Video');
+export const upload = (req, res) => res.send('Upload Video');
