8f1847c76c780bcc2da48f942b61106bd74a4eb6
Lilian Lee <lilianlee0213@gmail.com>
2023-02-02 13:36:26 -0500

    Iterations and Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,29 @@
-export const trending = (req, res) => res.render('home', {pageTitle: 'Home'});
+export const trending = (req, res) => {
+	const videos = [
+		{
+			title: 'first Video',
+			rating: 2,
+			comments: 43,
+			createdAt: '2 miniutes ago',
+			views: 242,
+		},
+		{
+			title: 'second Video',
+			rating: 2,
+			comments: 43,
+			createdAt: '2 miniutes ago',
+			views: 352,
+		},
+		{
+			title: 'third Video',
+			rating: 2,
+			comments: 43,
+			createdAt: '2 miniutes ago',
+			views: 42,
+		},
+	];
+	return res.render('home', {pageTitle: 'Home', videos});
+};
 export const see = (req, res) => res.render('watch', {pageTitle: 'Watch'});
 export const edit = (req, res) => {
 	return res.send('Edit');
