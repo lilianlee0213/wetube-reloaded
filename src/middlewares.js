d41b429876c0256b41be73225168f63e8f7f05fd
Lilian Lee <lilianlee0213@gmail.com>
2023-03-26 11:18:11 -0400

    multer s3

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,5 +1,17 @@
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
 
+const s3 = new aws.S3({
+	credentials: {
+		accessKeyId: process.env.AWS_ID,
+		secretAccessKey: process.env.AWS_SECRET,
+	},
+});
+const multerUploader = multerS3({
+	s3: s3,
+	bucket: 'wetubeclone2023',
+});
 export const localsMiddleware = (req, res, next) => {
 	res.locals.siteName = 'Wetube';
 	res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -29,8 +41,10 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const avatarUpload = multer({
 	dest: 'uploads/avatars',
 	limits: {fileSize: 3000000},
+	storage: multerUploader,
 });
 export const videoUpload = multer({
 	dest: 'uploads/videos',
 	limits: {fileSize: 100000000},
+	storage: multerUploader,
 });
