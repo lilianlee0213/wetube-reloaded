2ebf41444c83a325bcc2f802bba8abdec68f82fe
Lilian Lee <lilianlee0213@gmail.com>
2023-03-26 17:53:13 -0400

    test

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.set('view engine', 'pug');
 app.set('views', process.cwd() + '/src/views');
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	// res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	next();
 });
