b8b2b8bd68d2b255b362bc9f37b1bd86556d4c66
Lilian Lee <lilianlee0213@gmail.com>
2023-03-27 02:02:08 -0400

    typo fix and crossOrigin

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	res.header('Cross-Origin-Embedder-Policy', 'require-corp');
-	res.header('Cross-Origin-Opener-Policy', 'same-origin');
+	// res.header('Cross-Origin-Opener-Policy', 'same-origin');
 	next();
 });
 app.use(logger);
